d1f27a2e92f272bdb8694a671f839f010fec54bb
Michael Wright <wrigh517@gmail.com>
2014-05-13 16:23:26 -0400

    added dropping N counts #395

--- sandbox/fastq-to-fasta.py
+++ sandbox/fastq-to-fasta.py
@@ -1,14 +1,14 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
-# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# Copyright (C) Michigan State University, 2009-2014. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt.
 # Contact: khmer-project@idyll.org
 #
 import sys
-sys.path.insert(0, '/u/t/dev/screed')
 import screed
 
+N_count = 0
 for n, record in enumerate(screed.open(sys.argv[1])):
     if n % 10000 == 0:
         print>>sys.stderr, '...', n
@@ -17,7 +17,10 @@ for n, record in enumerate(screed.open(sys.argv[1])):
     name = record['name']
 
     if 'N' in sequence:
+        N_count += 1
         continue
 
     print ">" + name
     print sequence
+
+print>>sys.stderr, str(N_count) + " lines were dropped for 'N's in sequences."
